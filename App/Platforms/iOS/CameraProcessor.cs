615b3c9e08025bfc55de264179eadabd9cbbabdd
Mikkel Lindgreen Bech <mlb@biometric.dk>
2023-12-07 15:17:00 +0100

    Updated with recommendations from @rolfbjarne

--- App/Platforms/iOS/CameraProcessor.cs
+++ App/Platforms/iOS/CameraProcessor.cs
@@ -17,78 +17,88 @@ namespace App
         public override void DidFinishProcessingPhoto(AVCapturePhotoOutput output, AVCapturePhoto photo, NSError? error)
         {
 
-            if (error != null)
+            try
             {
-                Console.WriteLine(error);
-                FinishedProcessingPhotoEvent?.Invoke(null, null);
-                return;
-            }
+                using var pool = new NSAutoreleasePool();
+                if (error != null)
+                {
+                    Console.WriteLine(error);
+                    FinishedProcessingPhotoEvent?.Invoke(null, null);
+                    return;
+                }
 
-            if (photo.CGImageRepresentation == null)
+                using var cgimagerep = photo.CGImageRepresentation;
+                if (cgimagerep == null)
+                {
+                    Console.WriteLine("Photo taken is null!");
+                    FinishedProcessingPhotoEvent?.Invoke(null, null);
+                    return;
+                }
+
+                using var rotatedCIImage = new CIImage(cgimagerep);
+                var rotatedImage = rotatedCIImage.CreateByApplyingOrientation(ImageIO.CGImagePropertyOrientation.Right);
+
+                using var uiimage = new UIImage(rotatedImage);
+                if (uiimage == null)
+                {
+                    Console.WriteLine("Could not create UIImage");
+                    FinishedProcessingPhotoEvent?.Invoke(null, null);
+                    return;
+                }
+
+                using var jpeg = uiimage.AsJPEG(0.5f);
+                var uiimagedata = jpeg?.ToArray();
+                if (uiimagedata == null)
+                {
+                    Console.WriteLine("UIImagedata is null");
+                    FinishedProcessingPhotoEvent?.Invoke(null, null);
+                    return;
+                }
+
+                using var hairmaskraw = photo.GetSemanticSegmentationMatte(AVSemanticSegmentationMatteType.Hair);
+
+                if (hairmaskraw == null)
+                {
+                    Console.WriteLine("Hairmask raw is null");
+                    FinishedProcessingPhotoEvent?.Invoke(uiimagedata, null);
+                    return;
+                }
+
+                using var cihairmask = new CIImage(hairmaskraw);
+                using var hairMask = cihairmask.CreateByApplyingOrientation(ImageIO.CGImagePropertyOrientation.Right);
+
+                if (hairMask == null)
+                {
+                    Console.WriteLine("Hairmask CIImage is null");
+                    FinishedProcessingPhotoEvent?.Invoke(uiimagedata, null);
+                    return;
+                }
+
+                using var hairmaskuiimage = new UIImage(hairMask);
+                if (hairmaskuiimage == null)
+                {
+                    Console.WriteLine("Could not create UIImage");
+                    FinishedProcessingPhotoEvent?.Invoke(uiimagedata, null);
+                    return;
+                }
+
+                using var hairmaskjpeg = hairmaskuiimage.AsJPEG(0.5f);
+                var hairmaskuiimagedata = hairmaskjpeg?.ToArray();
+
+                if (hairmaskuiimagedata == null)
+                {
+                    Console.WriteLine("UIImagedata is null");
+                    FinishedProcessingPhotoEvent?.Invoke(null, null);
+                    return;
+                }
+
+                FinishedProcessingPhotoEvent?.Invoke(uiimagedata, hairmaskuiimagedata);
+
+            }
+            finally
             {
-                Console.WriteLine("Photo taken is null!");
-                FinishedProcessingPhotoEvent?.Invoke(null, null);
-                return;
+                photo.Dispose();
             }
-
-            var rotatedImage = new CIImage(photo.CGImageRepresentation).CreateByApplyingOrientation(ImageIO.CGImagePropertyOrientation.Right);
-
-            var uiimage = new UIImage(rotatedImage);
-
-            if (uiimage == null)
-            {
-                Console.WriteLine("Could not create UIImage");
-                FinishedProcessingPhotoEvent?.Invoke(null, null);
-                return;
-            }
-
-            var uiimagedata = uiimage.AsJPEG(0.5f)?.ToArray();
-
-            if (uiimagedata == null)
-            {
-                Console.WriteLine("UIImagedata is null");
-                FinishedProcessingPhotoEvent?.Invoke(null, null);
-                return;
-            }
-
-            var hairmaskraw = photo.GetSemanticSegmentationMatte(AVSemanticSegmentationMatteType.Hair);
-
-            if (hairmaskraw == null)
-            {
-                Console.WriteLine("Hairmask raw is null");
-                FinishedProcessingPhotoEvent?.Invoke(uiimagedata, null);
-                return;
-            }
-
-            var hairMask = new CIImage(hairmaskraw).CreateByApplyingOrientation(ImageIO.CGImagePropertyOrientation.Right);
-
-            if (hairMask == null)
-            {
-                Console.WriteLine("Hairmask CIImage is null");
-                FinishedProcessingPhotoEvent?.Invoke(uiimagedata, null);
-                return;
-            }
-
-            var hairmaskuiimage = new UIImage(hairMask);
-
-            if (hairmaskuiimage == null)
-            {
-                Console.WriteLine("Could not create UIImage");
-                FinishedProcessingPhotoEvent?.Invoke(uiimagedata, null);
-                return;
-            }
-
-            var hairmaskuiimagedata = hairmaskuiimage.AsJPEG(0.5f)?.ToArray();
-
-            if (hairmaskuiimagedata == null)
-            {
-                Console.WriteLine("UIImagedata is null");
-                FinishedProcessingPhotoEvent?.Invoke(null, null);
-                return;
-            }
-
-            // Send the photo back to the showcasepage
-            FinishedProcessingPhotoEvent?.Invoke(uiimagedata, hairmaskuiimagedata);
         }
     }
 }
