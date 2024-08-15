dcd7e8dabd2556f96cbdd8c745186320327b5684
Mikkel Lindgreen Bech <mlb@biometric.dk>
2024-08-15 14:54:53 +0200

    Added comments

--- App/Platforms/iOS/CameraPreview.cs
+++ App/Platforms/iOS/CameraPreview.cs
@@ -1,5 +1,8 @@
 namespace App
 {
+    /// <summary>
+    /// Simple view that will render the camera preview.
+    /// </summary>
     public class CameraPreview : View
     {
         public CameraPreview()
